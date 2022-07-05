7885241bfa44ba0d4873fc5a30bf00250bbe9256
Elgilany Hassan <gellanyhassan0@gmail.com>
2022-07-05 14:21:42 +0000

    Update gellany_nvm.sh

--- gellany_nvm.sh
+++ gellany_nvm.sh
@@ -3,3 +3,6 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 nvm install node
 node  -v
+npm install -g yarn
+yarn global add create-react-app
+create-react-app my-app
